a6d7e1e5b3de01616888c9c46692bc5b41e01042
wisnu kurniawan <wisnuko13@gmail.com>
2019-10-18 17:24:16 +0700

    Use attachBaseContext

--- app/src/main/java/com/wisnu/language/MainActivity.kt
+++ app/src/main/java/com/wisnu/language/MainActivity.kt
@@ -1,30 +1,24 @@
 package com.wisnu.language
 
-import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    applyLanguage(this, "in")
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    tv_language1.text = getString(R.string.bahasa)
-    tv_language2.text = applicationContext.getString(R.string.bahasa)
-  }
+        tv_language1.text = getString(R.string.bahasa)
+        tv_language2.text = applicationContext.getString(R.string.bahasa)
+    }
 
-  private fun applyLanguage(activity: Activity, language: String) {
-    val locale = Locale(language)
-    Locale.setDefault(locale)
-    val configuration = activity.baseContext.resources.configuration
-    configuration.setLocale(locale)
-    activity.baseContext.resources.updateConfiguration(configuration, activity.baseContext.resources.displayMetrics)
-    activity.baseContext.applicationContext.resources.updateConfiguration(configuration, activity.baseContext.resources.displayMetrics)
-  }
 
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(LocalizationUtil.applyLanguage(newBase, "th"))
+//        super.attachBaseContext(LocalizationUtil.applyLanguage(newBase, "id"))
+    }
 }
